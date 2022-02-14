2084070c0e608fa0ec84301ebc9ef7cdb2242743
DevidurgaArunachalam <96167015+DevidurgaArunachalam@users.noreply.github.com>
2022-02-14 16:08:33 +0530

    changed functionality

--- src/main/java/com/employee/model/Employee.java
+++ src/main/java/com/employee/model/Employee.java
@@ -8,76 +8,76 @@ import java.sql.Date;
 public class Employee {
    
     /**
-	 * Represents employee name, salary, contact number,and dateOfBirth.
-	 */
-	private int  employeeId ;
-	private String employeeName;
-	private String salary;
-	private String contactNumber;
-	private Date dateOfJoining;
+     * Represents employee name, salary, contact number,and dateOfBirth.
+     */
+    private int  employeeId ;
+    private String employeeName;
+    private String salary;
+    private String contactNumber;
+    private Date dateOfJoining;
 	
-	public Employee() {
-		super();
-	}
+    public Employee() {
+	super();
+    }
 
     /**
-	 * This Constructor creates an employee data
-	 * @param employeeId      
-	 * @param employeeName    
-	 * @param salary          
-	 * @param contactNumber         
-	 * @param dateOfBirth            
-	 */
-	public Employee (int  employeeId, String employeeName, String salary, String contactNumber, Date dateOfJoining) {
-		this.employeeId = employeeId;
-		this.employeeName = employeeName;
-		this.salary = salary;
-		this.contactNumber = contactNumber;
-		this.dateOfJoining = dateOfJoining;
-	}
+     * This Constructor creates an employee data
+     * @param employeeId      
+     * @param employeeName    
+     * @param salary          
+     * @param contactNumber         
+     * @param dateOfBirth            
+     */
+    public Employee (int  employeeId, String employeeName, String salary, String contactNumber, Date dateOfJoining) {
+	this.employeeId = employeeId;
+	this.employeeName = employeeName;
+	this.salary = salary;
+	this.contactNumber = contactNumber;
+	this.dateOfJoining = dateOfJoining;
+    }
 
-	public int getEmployeeId() {
-		return employeeId;
-	}
+    public int getEmployeeId() {
+	return employeeId;
+    }
 
-	public String getEmployeeName() {
-		return employeeName;
-	}
+    public String getEmployeeName() {
+	return employeeName;
+    }
 
-	public String getSalary() {
-		return salary;
-	}
+    public String getSalary() {
+	return salary;
+    }
 
-	public String getContactNumber() {
-		return contactNumber;
-	}
+    public String getContactNumber() {
+	return contactNumber;
+    }
 
-	public Date getDateOfJoining() {
-		return dateOfJoining;
-	}
+    public Date getDateOfJoining() {
+	return dateOfJoining;
+    }
 
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
+    public void setEmployeeId(int employeeId) {
+	this.employeeId = employeeId;
+    }
 	
-	public void setEmployeeName(String employeeName) {
-		this.employeeName = employeeName;
-	}
+    public void setEmployeeName(String employeeName) {
+	this.employeeName = employeeName;
+    }
 
-	public void setSalary(String salary) {
-		this.salary = salary;
-	}
+    public void setSalary(String salary) {
+	this.salary = salary;
+    }
 
-	public void setContactNumber(String contactNumber) {
-		this.contactNumber = contactNumber;
-	}
+    public void setContactNumber(String contactNumber) {
+	this.contactNumber = contactNumber;
+    }
 
-	public void setDateOfJoining(Date dateOfBirth) {
-		this.dateOfJoining = dateOfBirth;
-	}
+    public void setDateOfJoining(Date dateOfBirth) {
+	this.dateOfJoining = dateOfBirth;
+    }
 	
-	public String toString() {
-		return new StringBuffer().append("\n").append("EmployeeId:").append(employeeId).append("\n").append("Name:").append(employeeName).append("\n").append("Salary:").append(salary).
-				append("\n").append("Contact Number:").append(contactNumber).append("\n").append("DateOfJoining:").append(dateOfJoining).append("\n").append("\n").toString();
-	}
+    public String toString() {
+	return new StringBuffer().append("\n").append("EmployeeId:").append(employeeId).append("\n").append("Name:").append(employeeName).append("\n").append("Salary:").append(salary).
+		append("\n").append("Contact Number:").append(contactNumber).append("\n").append("DateOfJoining:").append(dateOfJoining).append("\n").append("\n").toString();
+    }
 }
